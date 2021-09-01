fb50a47b774d118319fae8b13158f0bbf82ab856
PabloCh95 <58525405+PabloCh95@users.noreply.github.com>
2021-09-01 15:13:30 -0300

    comitt inicial

---
+++ SuperHero/src/api/config.tsx
@@ -0,0 +1,3 @@
+export const token = '10159465720394500';
+
+export const URI = 'https://superheroapi.com/api/';
---
+++ SuperHero/src/navigation/navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import from '@react-navigation/native'
+
+export default function navigation() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
